ff3fa86d19abb3c2987b7b8f320156a67d665abd
Alex <agordon1123@gmail.com>
2019-07-19 13:35:45 -0400

    logo added

--- starwars/src/components/CreateCard.js
+++ starwars/src/components/CreateCard.js
@@ -2,11 +2,15 @@ import React from 'react';
 import styled from 'styled-components';
 
 const CardClass = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
     border: 2px solid black;
     background-color: #fafafa;
     border-radius: 5px;
-    height: 500px;
-    width: 300px;
+    height: 400px;
+    width: 220px;
     margin: 20px;
 `;
 
@@ -22,6 +26,7 @@ const CreateCard = (props) => {
             <p>Birthyear: {props.character.birth_year}</p>
             <p>Height: {props.character.height}</p>
             <p>Mass: {props.character.mass}</p>
+            <img src="https://img.icons8.com/color/96/000000/star-wars.png"></img>
         </CardClass>
     )
 }
